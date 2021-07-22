bebb632fbbc1f7b9bd2d5c05af3b3204f0f26f15
ekardum <emkardum@gmail.com>
2021-07-22 12:03:40 +0200

    nove promjene klasa na youtube-u

--- start.js
+++ start.js
@@ -1,5 +1,7 @@
-var Nightmare = require('nightmare');   
-var nightmare = Nightmare({ show: true });
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({
+    show: true
+});
 var getYouTubeID = require('get-youtube-id');
 var opn = require('opn');
 var fs = require('fs');
@@ -7,53 +9,64 @@ let text = fs.readFileSync("./songlist.txt").toString('utf-8');
 var urls = text.split("\n")
 var playlist = "https://www.youtube.com/watch_videos?video_ids="
 
-
 urls.reduce(function(accumulator, url) {
-  return accumulator.then(function(results) {
-    return nightmare.goto('https://youtube.com')
-      .wait('body')
-      .evaluate(function() {
-       document.querySelector('input#search').value =''
-      })
-      .type('input#search', url)
-      .wait('body')
-      .click('button#search-icon-legacy')
-      .wait(1000)
-      .evaluate(function () {
-       return document.querySelector('.ytd-two-column-search-results-renderer #contents .ytd-item-section-renderer #video-title').href;
-      })
-      .then(function(result){
-        results.push(result);
-        return results;
+    return accumulator.then(function(results) {
+        return nightmare.goto('https://youtube.com')
+            .wait('body')
+            .evaluate(function() {
+                if (document.body.contains(document.querySelector('button[jsname="higCR"]'))) {
+                    document.querySelector('button[jsname="higCR"]').click();
+                }
+            })
+            .wait('input#search')
+            .evaluate(function() {
+                document.querySelector('input#search').value = ''
+            })
+            .type('input#search', url)
+            .wait('body')
+            .click('button#search-icon-legacy')
+            .wait(1000)//.wait('.ytd-search') //.wait(1000)
+            .evaluate(function() {
+                return document.querySelector('.ytd-two-column-search-results-renderer #contents .ytd-item-section-renderer #video-title').href;
+            })
+            .then(function(result) {
+                results.push(result);
+                return results;
 
-      });
-  });
-}, Promise.resolve([])).then(function(results){
-   /*console.dir(results)*/;
+            })
+            .catch(function() {
+                console.log("Malo je zapelo na " || url);
+            });
+    });
+}, Promise.resolve([])).then(function(results) {
+    /*console.dir(results)*/
+    ;
 
     //For array output
     //fs.writeFileSync('songsList.json', JSON.stringify(results));
 
     results.forEach(function(url) {
-    var id = getYouTubeID(url);
-    playlist += id +",";
-    //console.log(playlist);
+        var id = getYouTubeID(url);
+        playlist += id + ",";
+        //console.log(playlist);
     });
     var file = fs.createWriteStream('links.txt');
-    file.on('error', function(err) { 'Error!' });
-    results.forEach(function(result) { file.write(result + "\r\n"); });
+    file.on('error', function(err) {
+        'Error!'
+    });
+    results.forEach(function(result) {
+        file.write(result + "\r\n");
+    });
     file.end();
     //console.log('Done!');
-}).then(function(){
-//results.forEach(function(url) {
-//    var id = getYouTubeID(url);
-//    var ids= playlist += id +",";
+}).then(function() {
+    //results.forEach(function(url) {
+    //    var id = getYouTubeID(url);
+    //    var ids= playlist += id +",";
     console.log(playlist);
     opn(playlist);
     console.log('Done!');
-  //});
-}).then(function() { 
-  return nightmare.end(); 
+    //});
+}).then(function() {
+    return nightmare.end();
 });
-
-
